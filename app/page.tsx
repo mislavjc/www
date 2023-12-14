b6ea89e1b67083337c0f6d6ee1a513eb1418b0d6
Mislav <mislavjc@gmail.com>
2023-12-15 00:58:10 +0100

    feat: add mt to main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Section } from 'components/section';
 
 const HomePage = () => {
   return (
-    <main className="mx-auto max-w-screen-md px-6">
+    <main className="mx-auto mt-16 max-w-screen-md px-6">
       <Section>
         <Hero />
       </Section>
