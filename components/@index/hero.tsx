f666e2a30aee546a99adbd9da2fecdeade67fccd
Mislav <68654622+mislavjc@users.noreply.github.com>
2023-12-15 20:27:28 +0100

    fix: typo in hero

--- components/@index/hero.tsx
+++ components/@index/hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
       <h1 className="font-grotesk text-5xl text-neutral-900 lg:text-7xl">
         Hi, I&apos;m <InflatedWord word="Mislav" />, crafting{' '}
         <i>
-          interacive experiences <Shape shape="2" />
+          interactive experiences <Shape shape="2" />
         </i>{' '}
         on the <u>web</u>.
       </h1>
